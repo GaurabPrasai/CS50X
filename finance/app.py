f7a0761b5392191ad12c3fc5f03078bc7f2d9ecd
Gaurab Prasai <78253021+GaurabPrasai@users.noreply.github.com>
2025-04-03 12:53:12 +0000

    Apr 3, 2025, 6:38 PM

--- finance/app.py
+++ finance/app.py
@@ -9,7 +9,7 @@ from helpers import apology, login_required, lookup, usd
 import datetime 
 # Configure application
 app = Flask(__name__)
-
+(((((())))))
 # Custom filter
 app.jinja_env.filters["usd"] = usd
 
