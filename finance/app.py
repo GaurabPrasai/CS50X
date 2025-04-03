c468a51f353afa0ce930ef6a9b02556c88c6c349
Gaurab Prasai <78253021+GaurabPrasai@users.noreply.github.com>
2025-04-03 12:51:04 +0000

    Apr 3, 2025, 6:36 PM

--- finance/app.py
+++ finance/app.py
@@ -6,7 +6,7 @@ from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import apology, login_required, lookup, usd
-import datetime
+import datetime 
 # Configure application
 app = Flask(__name__)
 
